da87a0337934ada703a51b92e834d15c1a0109fb
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-03-30 00:26:08 +0900

    fetch products with graphql

--- next.config.js
+++ next.config.js
@@ -3,9 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
-    SHOPIFY_STOREFRONT_API_KEY: process.env.SHOPIFY_STOREFRONT_API_KEY,
-    SHOPIFY_STOREFRONT_API_SECRET_KEY:
-      process.env.SHOPIFY_STOREFRONT_API_SECRET_KEY
+    SHOPIFY_STOREFRONT_ACCESSTOKEN:
+      process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
   },
   images: {
     domains: ['cdn.shopify.com']
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import ProductCard from "../components/ProductCard"
+import { getAllProducts } from "../lib/shopify"
 
-export default function Home() {
-  return <div className='text-xl text-blue'>Hello from Shopify Next.js</div>
+export default function Home({ products }) {
+
+  console.log(products);
+  return (
+    <div className="text-3xl">
+      Hello from Shopify Next.js
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const products = await getAllProducts()
+
+  return {
+    props: { products }, // will be passed to the page component as props
+  }
 }
