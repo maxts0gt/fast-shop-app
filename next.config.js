6838bf0eef8bc7d17af22d28ddd7bba0cae7faa3
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-03-29 22:00:12 +0900

    initial setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_API_KEY: process.env.SHOPIFY_STOREFRONT_API_KEY,
+    SHOPIFY_STOREFRONT_API_SECRET_KEY:
+      process.env.SHOPIFY_STOREFRONT_API_SECRET_KEY
+  },
+  images: {
+    domains: ['cdn.shopify.com']
+  }
 }
 
 module.exports = nextConfig
